8cfaa65db1eeddc34d5b3c8db458051ec5e65552
Selly Melananda <melanand4@gmail.com>
2023-07-20 11:49:57 +0700

    multer

--- middleware/multer.js
+++ middleware/multer.js
@@ -4,11 +4,11 @@ const path = require('path');
 // Konfigurasi penyimpanan file dengan multer
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      // Tentukan direktori penyimpanan file
+      // direktori penyimpanan file
       cb(null, 'images/');
     },
     filename: function (req, file, cb) {
-      // Tentukan nama file yang disimpan (disesuaikan dengan ekstensi gambar)
+      // nama file yang disimpan (disesuaikan dengan ekstensi gambar)
       cb(null, Date.now() + path.extname(file.originalname));
     }
   });
