045cc99b312f926f38342817d6b4f22e3ee77ef1
Selly Melananda <melanand4@gmail.com>
2023-07-25 14:29:36 +0700

    middleware

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,2 +1,27 @@
-const usersSchema = require ('../models/usersSchema');
+const Users = require ('../models/usersSchema');
 
+const isSuperAdmin = async (req, res, next)=> {
+    if(Users.role=='1'){
+        next()
+    }else{
+    res.status(401).json({
+        status:"failed",
+        message:"Hanya bisa diakses user Super Admin"
+    })
+    }
+}
+const isAdmin = async (req, res, next)=> {
+    if(Users.role=='2'){
+        next()
+    }else{
+    res.status(401).json({
+        status:"failed",
+        message:"Hanya bisa diakses user Admin"
+    })
+    }
+}
+
+module.exports = {
+    isSuperAdmin,
+    isAdmin
+}
